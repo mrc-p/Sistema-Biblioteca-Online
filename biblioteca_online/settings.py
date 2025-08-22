e5104e9a9f9b918ef86a4a64f859b6c9467d33bb
mrc-p <marcos.paulo.spires@gmail.com>
2025-08-22 11:58:00 -0300

    Att Settings e DATABASE_URL do novo banco de dados

--- biblioteca_online/settings.py
+++ biblioteca_online/settings.py
@@ -95,7 +95,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DATABASES = {
     'default': dj_database_url.parse(
-        config('DATABASE_URL'),
+        os.environ.get('DATABASE_URL', ''),
         conn_max_age=600,
         ssl_require=True
     )
